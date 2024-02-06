c724a1a0d92f2730d20a55ceb2fe3066bb984691
Ömer <omerincalan@gmail.com>
2024-02-06 11:27:56 -0800

    Update assignment4.py
    I am adding output and logicgate classes

--- assignment4.py
+++ assignment4.py
@@ -18,3 +18,42 @@ class Input:
         if self._owner is not None:
             self._owner.evaluate()
 
+class Output:
+    def __init__(self):
+        self._value = None  # Initially, the output value is undefined
+        self._connections = []  # For extra credit: List to hold connected inputs
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = bool(new_value)  # Convert the new value to a boolean
+        # For extra credit: Propagate the new value to connected inputs
+        for input in self._connections:
+            input.value = self._value
+
+    # For extra credit: Method to connect this output to an input
+    def connect(self, input_):
+        if input_ not in self._connections:
+            self._connections.append(input_)
+            # Immediately update the connected input's value to match this output's value
+            if self._value is not None:
+                input_.value = self._value
+class LogicGate:
+    def __init__(self, name):
+        self._name = name
+        self._output = Output()  # Every gate has an output
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def output(self):
+        return self._output
+
+    def evaluate(self):
+        pass  # This method will be overridden by subclasses to implement specific logic
+
