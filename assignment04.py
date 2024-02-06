151bdbddd9cfe199aac13ffbf3246197e59d800f
Ömer <omerincalan@gmail.com>
2024-02-06 14:17:05 -0800

    Update assignment04.py
    LogicGate Class implemented

--- assignment04.py
+++ assignment04.py
@@ -1,1 +1,21 @@
+class LogicGate:
+    def __init__(self, name):
+        self._name = name
+        self._output = None  # Will be initialized in child classes
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def output(self):
+        return self._output
+
+    def __str__(self):
+        # Implement a way to represent the gate's status as a string
+        pass
+
+    def evaluate(self):
+        # This method will be overridden by child classes to implement specific gate logic
+        pass
 
